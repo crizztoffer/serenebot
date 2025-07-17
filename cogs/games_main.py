a708950efa72cdb9513f3ddb373fca88530de02f
Crizz <darynalsup@gmail.com>
2025-07-17 12:01:05 -0700

    Update games_main.py

--- cogs/games_main.py
+++ cogs/games_main.py
@@ -143,3 +143,4 @@ async def setup(bot):
     await bot.add_cog(cog)
     # Explicitly add the game command to the serene_group, passing callback as a keyword argument
     serene_group.add_command(app_commands.Command(callback=cog.game_command_impl, name="game", description="Start a fun game with Serene!"))
+
